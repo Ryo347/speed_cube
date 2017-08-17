b3442b498d0fed8c40b76a889053a324957110cd
Ryota <ryosk347@gmail.com>
2017-08-17 15:52:35 +0000

    Add Scramble function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    
+    #回転記号をランダムに値を取り出す
+    def getScramble
+       symbol = ["F","F2","F'","U","U2","U'","R","R2","R'","L","L2","L2","B","B2","B'","D","D2","D'"]
+       15.time{
+           scrList.push(symbol.sample)
+       }
+       p scrList
+    end
 end
