d6140160a46d23ccc9a658ebc32925c133b3eae8
Ryota <ryosk347@gmail.com>
2017-08-20 16:15:03 +0000

    Update Scramble Function & Add Time measurement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,12 @@ module ApplicationHelper
     
     #回転記号をランダムに値を取り出す
     def getScramble
+       scrList = []
        symbol = ["F","F2","F'","U","U2","U'","R","R2","R'","L","L2","L2","B","B2","B'","D","D2","D'"]
-       15.time{
+       15.times {
            scrList.push(symbol.sample)
        }
+       
        p scrList
     end
 end
